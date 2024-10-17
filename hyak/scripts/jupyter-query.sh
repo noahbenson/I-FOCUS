bebe981d7834b112edc2857f3ec32f686c3c93f3
Noah C. Benson <nben@uw.edu>
2024-10-16 19:20:32 -0700

    tweaking screen configurations

--- hyak/scripts/jupyter-query.sh
+++ hyak/scripts/jupyter-query.sh
@@ -8,6 +8,9 @@ eval "$(${BINDIR}/hyak-jupyter-args.sh "$@")"
 [ -r "${JOB_FILE}" ] || exit 1
 source "${JOB_FILE}"
 
+# If the socket file isn't there, the job is over.
+[ -r "${SOCKET_FILE}" ] || exit 1
+
 # Make sure this is still our job...
 LNS=$(squeue -j "${SLURM_JOB_ID}" -u "${USER}" 2>/dev/null | wc -l)
 [ "$LNS" -eq 2 ] || exit 1
